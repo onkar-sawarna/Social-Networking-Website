20b79c9135fd3d98800f7ab42aa4a676d2a51234
onke21 <50941915+onke21@users.noreply.github.com>
2021-05-19 22:01:24 +0530

    tokens added to upload image

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -25,7 +25,6 @@ export const Signin = () => {
         }).then(res=>res.json())
         .then(data=>{
             if(data.error){
-                console.log(data.error);
                 //Mo.toast({html:data.error,classes:"#c62828 red darken-3"})
             }else{
                 Mo.toast({html:"Signed In successfull",classes:"#4caf50 green"})
